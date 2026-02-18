f57181aa425254b3540df1c04ebd2ab74a9fabd6
Niknameflame <saniavacepon@gmail.com>
2026-02-18 13:47:55 +0300

    en language fully, better code structure, owner verify moved to bot functional with ID

--- AsyncBot.py
+++ AsyncBot.py
@@ -1,36 +1,47 @@
 import asyncio
 import os
+import BotFunctional
 from telebot.async_telebot import AsyncTeleBot
 from telebot import types
 from dotenv import load_dotenv
 load_dotenv("Bot.env")
 
 Bot_token = str(os.getenv("Key"))
-Myself = int(os.getenv("ID"))
 bot = AsyncTeleBot(Bot_token)
+FunctionsList = {
+    "Shutdown pc🚫": {"msg": "Shutdown pc...", "func": BotFunctional.ShutdownPc},
+    "Lock pc🔄": {"msg": "Lock screen...", "func": BotFunctional.LockPc},
+    "Take screenshot📺": {"msg": "Taked screenshoot...", "func": BotFunctional.TakeScreenshot},
+}
 
 @bot.message_handler(commands=["start"])
 async def AlreadyConnected(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    TestFunc1 = types.KeyboardButton("Untitled 1")
-    TestFunc2 = types.KeyboardButton("Untitled 2")
-    TestFunc3 = types.KeyboardButton("Выключение пк🚫")
+    TestFunc1 = types.KeyboardButton("Lock pc🔄")
+    TestFunc2 = types.KeyboardButton("Take screenshot📺")
+    TestFunc3 = types.KeyboardButton("Shutdown pc🚫")
     markup.row(TestFunc1,TestFunc2)
     markup.row(TestFunc3)
 
-    if message.from_user.id == Myself:
-        await bot.send_message(message.chat.id,"Здравствуйте создатель🙂", reply_markup=markup)     
+    if BotFunctional.Check(message.from_user.id):
+        await bot.send_message(message.chat.id,"Hello owner🙂", reply_markup=markup)     
     else:
-        await bot.send_message(message.chat.id,"Здравствуйте, пока что бот находится в стадии разработки и не работает ни у кого, кроме создателя")
+        await bot.send_message(message.chat.id,"Sorry, bot created only for owner!")
 
 @bot.message_handler(content_types="text")
 async def Commands(message: types.Message):
-    await bot.reply_to(message, "Проверка...")
-    if message.from_user.id == Myself:
-        if message.text == "Выключение пк🚫":
-            await bot.reply_to(message, "Выключение ПК...")
-            os.system("shutdown /s /f /t 0")
-        else:
-            await bot.reply_to(message, "Что-то не так...")
+    await bot.reply_to(message, "Check...")
+
+    if BotFunctional.Check(message.from_user.id):
+        bot_function = FunctionsList[message.text]
+        await bot.send_message(message.chat.id, bot_function["msg"])
+
+        result = bot_function["func"]()
+        if str(result).endswith(".png"):
+            with open(result, "rb") as photo:
+                await bot.send_photo(message.chat.id,photo)
+            os.remove(rf"D:\\Coding\\TelegramBot\\DistantControlBot\\{result}")
+    else:
+        bot.send_message(message.chat.id, "You are not owner.")
 
 asyncio.run(bot.infinity_polling())
---
+++ BotFunctional.py
@@ -0,0 +1,22 @@
+import os
+import pyautogui as gui
+from dotenv import load_dotenv
+load_dotenv("Bot.env")
+Myself = int(os.getenv("ID"))
+
+def Check(SendingID):
+    if SendingID == Myself:
+        return True
+    else:
+        return False
+
+def ShutdownPc():
+    os.system("shutdown /s /f /t 0")
+
+def LockPc():
+    os.system("rundll32.exe user32.dll,LockWorkStation")
+
+def TakeScreenshot():
+    path = "Screen.png"
+    gui.screenshot(path)
+    return path
